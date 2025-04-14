73f15af279283d6c7680fa6792ee64c7cd22f164
sagardhavade <sagardhavade1998@gmail.com>
2025-04-15 00:06:09 +0530

    resolve Map isue

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,6 +1,6 @@
 import AboutTitleSection from "../components/about/AboutTitleSection";
 import Contact from "../components/Contact";
-import Map from "../components/map";
+import Map from "../components/Map";
 const ContactPage = () =>{
     return(<>
     <AboutTitleSection title={"Contact"} breadcrumbItem={"Contact"} />
